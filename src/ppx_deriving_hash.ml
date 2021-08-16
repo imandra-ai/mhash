cb78a387524d16e26477721c41ce02c6797638e8
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-08-16 09:38:57 -0400

    hash floats

--- src/ppx_deriving_hash.ml
+++ src/ppx_deriving_hash.ml
@@ -74,6 +74,10 @@ let rec hash_expr_of_type (e:expression) ~(ty:core_type) : expression =
   | [%type: bytes] -> by_hasher [%expr Ppx_deriving_hash_runtime.bytes]
   | [%type: bool] -> by_hasher [%expr Ppx_deriving_hash_runtime.bool]
   | [%type: unit] -> by_hasher [%expr Ppx_deriving_hash_runtime.trivial]
+  | [%type: float] ->
+    (* hash float as its bitwise representation (an int64) *)
+    apply_hasher ~loc [%expr Ppx_deriving_hash_runtime.int64]
+      [%expr Int64.bits_of_float [%e e]]
 
   | [%type: [%t? ty_arg0] option] ->
     [%expr
